c9b44d66c8bd4fe00e257cfcf8a55f1cc6b091d2
jaceshim <jace@192.168.223.203>
2013-06-28 12:05:43 +0900

    작업사항 반영함.

---
+++ src/main/java/randy/web/domain/CategoryTagUnreg.java
@@ -0,0 +1,73 @@
+package randy.web.domain;
+
+import randy.core.spring.domain.AbstractDomain;
+
+/**
+ * 카테고리 미 등록 태그.
+ * 
+ * @author jace
+ */
+public class CategoryTagUnreg extends AbstractDomain {
+
+	/** 순번 */
+	private Integer seq;
+	/** 몰 아이디 */
+	private Integer mallId;
+	/** 상품명 */
+	private String prdName;
+	/** 태그 */
+	private String tag;
+	/** 처리여부 */
+	private String procYn;
+	/** 등록일시 */
+	private String regDt;
+
+	public Integer getSeq() {
+		return seq;
+	}
+
+	public void setSeq(Integer seq) {
+		this.seq = seq;
+	}
+
+	public Integer getMallId() {
+		return mallId;
+	}
+
+	public void setMallId(Integer mallId) {
+		this.mallId = mallId;
+	}
+
+	public String getPrdName() {
+		return prdName;
+	}
+
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getProcYn() {
+		return procYn;
+	}
+
+	public void setProcYn(String procYn) {
+		this.procYn = procYn;
+	}
+
+	public String getRegDt() {
+		return regDt;
+	}
+
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+
+}
