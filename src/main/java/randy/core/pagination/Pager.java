ee1fd9117fae326e8348535bf4f1c750550490c8
jaceshim <jace@192.168.223.203>
2013-07-28 06:10:56 +0900

    pager 변수명 변경작업 반영.

--- src/main/java/randy/core/pagination/Pager.java
+++ src/main/java/randy/core/pagination/Pager.java
@@ -24,9 +24,9 @@ public class Pager<T> implements Serializable {
 	/** 총 개수 */
 	private int totalCount;
 	/** 페이지 번호가 화면에 보여질 개수 ex) [1], [2], [3].. [10] */
-	private int pageCount;
+	private int pageUnit;
 	/** 한 페이지에 보여질 row 개수 */
-	private int rowCount;
+	private int pageSize;
 	/** 최대 페이지 번호 (전체 페이지 개수) */
 	private int maxPage;
 	/** 화면에 보여지는 페이지번호의 처음 페이지번호 */
@@ -42,8 +42,90 @@ public class Pager<T> implements Serializable {
 		this(items, currentPage, totalCount, rowCount, DEFAULT_PAGE_COUNT);
 	}
 
-	public Pager(Collection<T> items, int currentPage, int totalCount, int rowCount, int pageCount) {
+	public Pager(Collection<T> items, int currentPage, int totalCount, int rowCount, int pageUnit) {
+		this.totalCount = totalCount;
+		this.rowCount = rowCount;
+		this.pageUnit = pageCount;
+		this.maxPage = rowCount == 0 ? this.totalCount : (totalCount - 1) / rowCount + 1;
+		this.currentPage = currentPage > maxPage ? maxPage : currentPage;
+		this.firstPage = ((currentPage - 1) / pageCount) * pageCount + 1;
+		this.lastPage = firstPage + (pageCount - 1);
+	}
 
+	public boolean hasNextPage() {
+		return currentPage < maxPage;
+	}
+
+	public boolean hasPreviousPage() {
+		return currentPage > 1;
+	}
+
+	public int getNextPage() {
+		return currentPage + 1;
+	}
+
+	public void setNextPage(int val) {
+		// not called
+	}
+
+	public int getPreviousPage() {
+		return currentPage - 1;
+	}
+
+	public void setPreviousPage(int val) {
+		// not called
+	}
+
+	public boolean hasNextPage() {
+		return lastPage < maxPage;
+	}
+
+	public boolean hasPrevPage() {
+		return currentPage >= pageCount + 1;
+	}
+
+	public int getStartOfNextPageUnit() {
+		return endUnitPage + 1;
+	}
+
+	public int getStartOfPreviousPageUnit() {
+		return beginUnitPage - 1;
+	}
+
+	public int getPageOfNextPageUnit() {
+		return (currentPage + pageunit < maxPage) ? currentPage + pageunit : maxPage;
+	}
+
+	public int getPageOfPreviousPageUnit() {
+		return (currentPage - pageunit > 1) ? currentPage - pageunit : 1;
+	}
+
+	public int getCurrentPage() {
+		return this.currentPage;
+	}
+
+	public int getBeginUnitPage() {
+		return this.beginUnitPage;
+	}
+
+	public int getEndListPage() {
+		return (endUnitPage > maxPage) ? this.maxPage : this.endUnitPage;
+	}
+
+	public void setEndListPage(int val) {
+		// not called.
+	}
+
+	public int getSize() {
+		return objects.size();
+	}
+
+	public boolean isEmptyPage() {
+		return (this.objects == null || this.getSize() == 0) ? true : false;
+	}
+
+	public void setEmptyPage(boolean val) {
+		// not called.
 	}
 
 	/*--------------------------------------------
